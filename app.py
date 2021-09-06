cc80abb7c704e6f389f74064f9d1bdc1cb73832f
gabrielmotroni-prog <60718584+gabrielmotroni-prog@users.noreply.github.com>
2021-09-06 18:05:38 -0300

    p2

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
-## API em Flask - Python
 
-#from flask import Flask, jsonify
-from flask import Flask
+# app.py
+from flask import Flask, jsonify
+from datetime import datetime
 
-#from datetime import datetime
-#from datetime import Datetime
-
-# objeto cosntrutor do flask
 app = Flask(__name__)
 
-
-#Rota Hello Word
-@app.route('/')
-@app.route('/index')
-def hello_word():
-    '''   return jsonify({
+@app.route("/")
+def helo_word():
+    return jsonify({
         'message:':'Hello Word! :D',
         'Author': 'Gabriel dos Santos Motroni',
-        'Today': 'datetime.now()'
-    }), 200'''
-    return "hello word!"
- 
+        'Today': datetime.utcnow()
+    }), 200
 
 
-#inicializador do flask
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+#para testar no bash:
+# FLASK_APP=app
+#flask run --host '0.0.0.0'
